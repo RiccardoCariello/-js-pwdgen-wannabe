c59bc475282bdbd888e2066027da5977c2480b25
MillionDollarBob <riccardo.c1995@outlook.com>
2023-03-28 16:08:41 +0200

    Esercizio Ultimato

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 console.log(`
 #######################################################
 Benvenuto nel nostro affidabilissimo Password-Generator
@@ -10,25 +12,35 @@ Benvenuto nel nostro affidabilissimo Password-Generator
 #######################################################
 `);
 
-let nome = prompt("Inserisci il tuo nome.");
+let nome = prompt("Inserisci il tuo nome.", "Pinco");
+document.getElementById("nomejs").innerHTML = nome; 
 
-let cognome = prompt("Inserisci il tuo cognome.");
+let cognome = prompt("Inserisci il tuo cognome.", "Pallino");
+document.getElementById("cognomejs").innerHTML = cognome; 
 
-let colore = prompt("Inserisci il tuo colore preferito.");
+let colore = prompt("Inserisci il tuo colore preferito.", "giallo");
+document.getElementById("colorejs").innerHTML = colore; 
 
 let primonumero = prompt("Inserisci un numero a tua scelta. " , "0");
 primonumero = parseFloat(primonumero);
+document.getElementById("num1js").innerHTML = primonumero; 
+console.log(primonumero);
 
 let secondonumero = prompt("Inserisci un'altro numero a tua scelta, possibilmente più piccolo del precedente.", "0");
 secondonumero = parseFloat(secondonumero);
+document.getElementById("num2js").innerHTML = secondonumero; 
+console.log(secondonumero);
 
 let pwcode = primonumero / secondonumero;
 pwcode = parseInt(pwcode);
+console.log(pwcode);
 
 let randonum = Math.random() * 99;
 randonum = parseInt(randonum);
+console.log(randonum);
 
 const password = nome + cognome + colore + pwcode + randonum;
+document.getElementById("passwordjs").innerHTML = password;  
 
 console.log(`
 #######################################
@@ -41,7 +53,6 @@ ${password}
 
 
 `);
-
 
 
 
